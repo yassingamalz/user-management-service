688d137975e069a8963c063bfa43fe8971c4648d
yassingamalz <yassen.gamal.yg@gmail.com>
2023-12-07 12:21:58 +0100

    controller(UserController): implement APIs

---
+++ src/main/java/com/logiclytics/usermanagementservice/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.logiclytics.usermanagementservice.controller;
+
+import com.logiclytics.usermanagementservice.model.User;
+import com.logiclytics.usermanagementservice.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserServiceImpl userService;
+
+    @Autowired
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+        return userService.updateUser(id, updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
