092b999bb6c7659981653e914370bfd977d25366
yassingamalz <yassen.gamal.yg@gmail.com>
2023-12-08 16:59:01 +0100

    repository(Role): create RoleRepository with custom methods

---
+++ src/main/java/com/logiclytics/usermanagementservice/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.logiclytics.usermanagementservice.repository;
+
+import com.logiclytics.usermanagementservice.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    boolean existsByRoleName(String roleName);
+}
