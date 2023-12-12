d584e3b03d61e8bc3bb554bf91f2f6df90e4f8f5
yassingamalz <yassen.gamal.yg@gmail.com>
2023-12-12 12:17:53 +0100

    repository(Role): add findByRoleName custom method

--- src/main/java/com/logiclytics/usermanagementservice/repository/RoleRepository.java
+++ src/main/java/com/logiclytics/usermanagementservice/repository/RoleRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
     boolean existsByRoleName(String roleName);
+
+    Role findByRoleName(String roleName);
 }
