ebb29dbc826d7abf80d9f26a4377ca61475e11cf
yassingamalz <yassen.gamal.yg@gmail.com>
2023-12-12 12:18:09 +0100

    repository(User): add findByUsername custom method

--- src/main/java/com/logiclytics/usermanagementservice/repository/UserRepository.java
+++ src/main/java/com/logiclytics/usermanagementservice/repository/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+    User findByUsername(String userName);
 }
